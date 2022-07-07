e9c12d1df0920d2cc12bfb5976dc1fa9343289ef
ISAAC ANTEPARA <69361351+isaacanteparac@users.noreply.github.com>
2022-07-07 17:08:56 -0500

    🧩

--- python/algoritmoGenetico.py
+++ python/algoritmoGenetico.py
@@ -1,7 +1,22 @@
 
 import random
 
+box = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+       [0, 7, 0, 0, 4, 0, 0, 0, 0, 3, 0],
+       [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],]
+
 nchromosomes = [[], [], [], [], [], []]
+
+start = box[3][1]
+initialPosition = 0
+
+
+
 
 def compass(n):
     if(n == 1):
@@ -27,25 +42,28 @@ def compass(n):
 def randomGenerator(n):
     global nchromosomes
     for i in range(8):
-        numberR = random.randint(1,8)
+        numberR = random.randint(1, 8)
         orientation = compass(numberR)
         if(numberR <= 4):
-            numberValue = (f"Position: {numberR}", "Weight: 10", f"Orientation: {orientation}")
+            numberValue = (numberR, 10, orientation)
         else:
-            numberValue = (f"Position: {numberR}",  "Weight: 15",  f"Orientation: {orientation}")
+            numberValue = (numberR, 15, orientation)
         nchromosomes[n].append(numberValue)
+
 
 def chromosomeGenerator():
     global nchromosomes
     for c in range(6):
         randomGenerator(c)
         print(f"------------------------{c}----------------------------\n")
+        print(f"N-RANDOM > WEIGHT > ORIENTATION")
         for p in range(len(nchromosomes[c])):
             print(f"{nchromosomes[c][p]}\n")
-       
+
 
 def main():
     print("\nGITHUB @isaacanteparac\n")
     chromosomeGenerator()
+
 
 main()
