9d7934262696ecf87442a7f1cd475782f0295f31
ISAAC ANTEPARA <69361351+isaacanteparac@users.noreply.github.com>
2022-07-09 02:37:29 -0500

    🧩

--- python/algoritmoGenetico.py
+++ python/algoritmoGenetico.py
@@ -10,15 +10,12 @@ box = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
        [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
        [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],]
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ]
 
 nchromosomes = [[], [], [], [], [], []]
 
-start = box[3][1]
 boxPosition = 3
 insideBox = 1
-
-
 
 
 def compass(n):
@@ -64,64 +61,76 @@ def chromosomeGenerator():
         for p in range(len(nchromosomes[c])):
             print(f"{nchromosomes[c][p]}")
 
+
 def think(subArray):
     global nchromosomes, boxPosition, insideBox
     for p in range(len(nchromosomes[subArray])):
         orientation = nchromosomes[subArray][p][2]
         walking(boxPosition, insideBox, orientation, subArray, p)
     boxGet()
-    
+   
+
 
 def walking(boxPosition, insideBox, orientation, subChromosome, position):
     global box, nchromosomes
     if(orientation == "N"):
-        boxPosition-=1
+        boxPosition -= 1
+        box[boxPosition][insideBox] += 1
+        print(f"{orientation} box{boxPosition}:{insideBox}")
+        nchromosomes[subChromosome][position][3] += 1
+    elif(orientation == "S"):
+        boxPosition += 1
+        box[boxPosition][insideBox] += 1
+        print(f"{orientation} box{boxPosition}:{insideBox}")
+        nchromosomes[subChromosome][position][3] += 1
+    elif(orientation == "E"):
+        insideBox += 1
         box[boxPosition][insideBox] = 1
         print(f"{orientation} box{boxPosition}:{insideBox}")
-        nchromosomes[subChromosome][position][3] +=1
-    elif(orientation == "S"):
-        boxPosition +=1
-        box[boxPosition][insideBox] += 1
-        nchromosomes[subChromosome][position][3] +=1
-    elif(orientation == "E"):
-        insideBox+=1
-        box[boxPosition][insideBox] = 1
-        nchromosomes[subChromosome][position][3] +=1
+        nchromosomes[subChromosome][position][3] += 1
     elif(orientation == "O"):
-        insideBox-=1
-        box[boxPosition][insideBox] = 1
-        nchromosomes[subChromosome][position][3] +=1
+        insideBox -= 1
+        box[boxPosition][insideBox] += 1
+        print(f"{orientation} box{boxPosition}:{insideBox}")
+        nchromosomes[subChromosome][position][3] += 1
     elif(orientation == "NO"):
-        boxPosition -=1
-        insideBox-=1
-        box[boxPosition][insideBox] = 1
-        nchromosomes[subChromosome][position][3] +=1
+        boxPosition -= 1
+        insideBox -= 1
+        box[boxPosition][insideBox] += 1
+        print(f"{orientation} box{boxPosition}:{insideBox}")
+        nchromosomes[subChromosome][position][3] += 1
     elif(orientation == "NE"):
-        boxPosition -=1
-        insideBox+=1
-        box[boxPosition][insideBox] = 1
-        nchromosomes[subChromosome][position][3] +=1
+        boxPosition -= 1
+        insideBox += 1
+        box[boxPosition][insideBox] += 1
+        print(f"{orientation} box{boxPosition}:{insideBox}")
+        nchromosomes[subChromosome][position][3] += 1
     elif(orientation == "SO"):
-        boxPosition +=1
-        insideBox-=1
-        box[boxPosition][insideBox] = 1
-        nchromosomes[subChromosome][position][3] +=1
+        boxPosition += 1
+        insideBox -= 1
+        box[boxPosition][insideBox] += 1
+        print(f"{orientation} box{boxPosition}:{insideBox}")
+        nchromosomes[subChromosome][position][3] += 1
     elif(orientation == "SE"):
-        boxPosition +=1
-        insideBox+=1
-        box[boxPosition][insideBox] = 1
-        nchromosomes[subChromosome][position][3] +=1
-    
-    
-
-
-
+        boxPosition += 1
+        insideBox += 1
+        box[boxPosition][insideBox] += 1
+        print(f"{orientation} box{boxPosition}:{insideBox}")
+        nchromosomes[subChromosome][position][3] += 1
 
 
 def boxGet():
     global box
     for i in range(len(box)):
-       print(f"{box[i]}")
+        print(f"{box[i]}")
+    box = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+       [0, 7, 0, 0, 4, 0, 0, 0, 0, 3, 0],
+       [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ]
 
 
 def main():
