cf9ddb55e42558a06094967fa55c0d6f74af1488
ISAAC ANTEPARA <69361351+isaacanteparac@users.noreply.github.com>
2022-07-14 00:36:16 -0500

    quicksort

--- python/o.py
+++ python/o.py
@@ -1,8 +1,35 @@
-entrada = [5,8,1,3,6]
-salidad = []
-mini = 0
-for i in range((len(entrada)-2)):
-    mini = i
-    for j in range((len(entrada)-1)):
-        if(entrada[j] < entrada[mini]):
-            #h
+
+import random
+
+from mysqlx import View
+array_ = []
+
+def quicksort_(array):
+    mini = 0
+    for i in range((len(array)-2)):
+        mini = i
+        for j in range(i+1, len(array)):
+            if(array[j] < array[mini]):
+                mini = j
+        array[mini], array[i] = array[i], array[mini]
+
+    return array
+
+def random_(n, start, end):
+    global array_
+    for i in range(n):
+        number = random.randint(start, end)
+        array_.append(number)
+    return array_
+
+def main():
+    global array_
+    aR = random_(300, 1, 1000) 
+    print(f"ARRAY DE NUMEROS RANDOMS\n{aR}")
+    qS = quicksort_(array_)
+    print(f"\nORDENAMIENTO DE NUMEROS\n{qS}")
+
+
+main()
+
+
