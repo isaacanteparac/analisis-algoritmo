e43df4b5de81f869769e3b8fee1b1c176cc418a1
ISAAC ANTEPARA <69361351+isaacanteparac@users.noreply.github.com>
2022-07-21 01:26:02 -0500

    OK

--- python/mergeSort.py
+++ python/mergeSort.py
@@ -4,39 +4,62 @@ import random
 array = []
 newArray = []
 nDiv = 0
+nPosition = 0
+nElement = 0
 
 
-def div():
-    global nDiv
-    global newArray, array
+def divN():
+    global newArray, array, nDiv
     countElementArray = len(array)
     for d in range(nDiv):
         newArray.append([])
-    pair = countElementArray % 2
     opDiv = countElementArray/nDiv
-    print(f"opt value {opDiv}")
-    if(pair == 0):
-        addElement(countElementArray, opDiv)
+    addElement(opDiv, 0, array, 0, countElementArray)
+
+
+def divTwo():
+    global newArray, nDiv, nPosition, nElement
+    newArray.append([])
+    countElement = len(newArray[nPosition])
+    #pair = countElement%2
+    print(f"positon de NEW ARRAY = {nPosition}")
+    if (nElement == countElement):
+        print(countElement)
+        nPosition +=1
+        print(f"positon de NEW ARRAY = {nPosition}")
     else:
-        opDiv = int(opDiv)
-        print(f"impar {opDiv}")
-        addElement(countElementArray, opDiv)
+        #if ( pair == 0):
+        div = countElement/2
+        div = int(div)
+        if(nPosition <= nDiv):
+            addElement(3, -1, newArray[nPosition], nElement, (nElement+1))
+        nElement += 1
+                
+        """else:
+            print("impar")
+            div = countElement/3
+            div = (int(div)+1)
+            print(div)
+            if(nPosition <= nDiv):
+                addElement(4, -1, newArray[nPosition], nElement, (nElement+1))"""
+        
+        divTwo()
+       
 
+    
 
-
-def addElement(countArray, nElementArray,):
-    global newArray, array
-    position = 0
+def addElement(nElementArray, startPosition, arrays, forStart, forEnd):
+    global newArray
+    position = startPosition
     count = 0
-    for ca in range(countArray):
+    for ca in range(forStart, forEnd):
         if(count < nElementArray):
-            newArray[position].append(array[ca])
+            newArray[position].append(arrays[ca])
             count +=1
         else:
             position +=1
-            newArray[position].append(array[ca])
+            newArray[position].append(arrays[ca])
             count = 0
-
 
 
 
@@ -48,6 +71,7 @@ def randomGenerator(nElements, nStart, nEnd):
     return array
 
 
+
 def main():
     global nDiv
     nDiv = int(input("numero de elemtos a dividir: "))
@@ -55,8 +79,19 @@ def main():
     nStart = int(input("numero inicial: "))
     nEnd = int(input(f"numero de rango final, mayor a {nStart}: "))
     print(f"{randomGenerator(nRandoms,nStart,nEnd)}")
-    div()
+    divN()
     print(newArray)
+    divTwo()
+    print(newArray)
+
+    
 
 
 main()
+
+#se define los array 
+#lo que hace es divir los elementos en elementos iguales
+#luego dentro de cada arreglo se van hacer divisiones
+#el dfs es recursivo y tiene un punto de parada
+
+#EXAMEN HACER EL CODIGO 
